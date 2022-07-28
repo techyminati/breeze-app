d4b4b92fa18e2c8bc6d7f1c8ef189752aabde046
techyminati <sinha.aryan03@gmail.com>
2022-07-28 14:08:48 +0530

    breeze: Inherit english_words package
    Change-Id: I6f6a3c0863de9aaf411faec88229db2da88c3842
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
